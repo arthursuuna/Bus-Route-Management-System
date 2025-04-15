f7bfd074c9c55cf35a379b3d38c8c1799302415b
arthursuuna <ssuunaarthur@gmail.com>
2025-04-15 16:34:38 +0300

    new change
    nc

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,18 +2,14 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <title>Bus Pass and Route Management system</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    @vite('resources/css/app.css') {{-- if using Vite + Tailwind --}}
 </head>
-<body>
-    
-{{-- <p>{{ route('contact') }}</p> --}}
-
-<h1>@yield('title')</h1>
-<p><a href="{{ route('home') }}">HOME</a></p>
-<p><a href="{{ route('about') }}">ABOUT US</a></p>
-
-
+<body class="bg-gray-100 text-gray-800">
+    @include('partials.navbar')
+    <main>
+        @yield('content')
+    </main>
 </body>
 </html>
